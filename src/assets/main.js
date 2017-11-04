946830e7c2fef06a580fbfba76ecc944877066ad
ekais <e.kaiser.93@gmail.com>
2017-11-04 15:31:33 -0500

    added all badges, added h3 tags, added a tags, and connected it to AJAX

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,9 @@
 $(function() {
-
-  // your code will go here
-
+https://www.codeschool.com/users/ekaiser93.json
+  $.ajax({
+    url: 'https://www.codeschool.com/users/ekaiser93.json',
+    dataType: 'jsonp', success: function(response) {
+      return(`response.courses.completed`);
+    }
+  });
 });
